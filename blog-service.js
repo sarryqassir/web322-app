f38456a0c4570316e0028475f9518675d83b39b9
Sarry-PC <sarryqassir@hotmail.com>
2022-06-19 17:05:59 -0400

    added cloudinary support

--- blog-service.js
+++ blog-service.js
@@ -54,3 +54,57 @@ module.exports.getCategories = function () {
     }
   });
 };
+
+module.exports.addPost = (postData) => {
+  return new Promise((resolve, reject) => {
+    if (postData.length === 0) {
+      reject();
+    }
+
+    if (postData.published === undefined) {
+      postData.published = false;
+    } else {
+      postData.published = true;
+    }
+    postData.id = posts.length + 1;
+    posts.push(postData);
+    resolve();
+  });
+};
+
+module.exports.getPostsByCategory = (category) => {
+  return new Promise((resolve, reject) => {
+    let fPosts = posts.filter((post) => post.category === category);
+
+    if (fPosts.length === 0) {
+      reject("No results returned");
+    } else {
+      resolve(fPosts);
+    }
+  });
+};
+
+module.exports.getPostsByMinDate = (minDateStr) => {
+  return new Promise((resolve, reject) => {
+    let minDate = posts.filter(
+      (post) => new Date(post.postDate) >= new Date(minDateStr)
+    );
+
+    if (minDate.length === 0) {
+      reject("No results returned");
+    } else {
+      resolve(minDate);
+    }
+  });
+};
+
+module.exports.getPostById = (id) => {
+  return new Promise((resolve, reject) => {
+    let idPosts = posts.filter((post) => post.id === id);
+    if (idPosts.length === 0) {
+      reject("No results returned");
+    } else {
+      resolve(idPosts);
+    }
+  });
+};
--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+ *  WEB322 – Assignment 02
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source
+ *  (including 3rd party web sites) or distributed to other students.
+ *
+ *  Name: Sarry Al-Qassir Student ID: 119161206 Date: June 19, 2022
+ *
+ *  Online (Heroku) Link: https://warm-shelf-44471.herokuapp.com/
+ *
+ ********************************************************************************/
+
 const blog = require("./blog-service");
 
 const express = require("express");
@@ -5,6 +16,21 @@ const express = require("express");
 const app = express();
 
 const path = require("path");
+
+const multer = require("multer");
+
+const upload = multer(); // no { storage: storage } since we are not using disk storage
+
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "sarry-seneca",
+  api_key: "382965665789623",
+  api_secret: "2MRfsVKsPm-Nbci6gOwgPJfyQ0k",
+  secure: true,
+});
+
+const streamifier = require("streamifier");
 
 app.use(express.static("public"));
 
@@ -34,14 +60,95 @@ app.get("/blog", (req, res) => {
 });
 
 app.get("/posts", (req, res) => {
+  let minDate = req.query.minDate;
+  let category = req.query.category;
+  console.log(minDate);
+
+  if (category) {
+    blog
+      .getPostsByCategory(parseInt(category))
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        console.log("message: " + err);
+      });
+  } else if (minDate) {
+    blog
+      .getPostsByMinDate(minDate)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        console.log("message: " + err);
+      });
+  } else {
+    blog
+      .getAllPosts()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        console.log("message: " + err);
+      });
+  }
+});
+
+app.get("/post/:value", (req, res) => {
+  let reqValue = req.params.value;
   blog
-    .getAllPosts()
-    .then((data) => {
-      res.json(data);
+    .getPostById(parseInt(reqValue))
+    .then((posts) => {
+      res.json(posts);
     })
     .catch((err) => {
       console.log("message: " + err);
     });
+});
+
+app.get("/posts/add", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/addPost.html"));
+});
+
+app.post("/posts/add", upload.single("featureImage"), (req, res) => {
+  if (req.file) {
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        });
+
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+
+    async function upload(req) {
+      let result = await streamUpload(req);
+      console.log(result);
+      return result;
+    }
+
+    upload(req)
+      .then((uploaded) => {
+        processPost(uploaded.url);
+      })
+      .catch((err) => {
+        console.log("message: " + err);
+      });
+  } else {
+    processPost("");
+  }
+
+  function processPost(imageUrl) {
+    req.body.featureImage = imageUrl;
+    blog.addPost(req.body).then((data) => {
+      res.redirect("/posts");
+    });
+  }
 });
 
 app.get("/categories", (req, res) => {
